5f7e6fc59ed5e6f7cdab3215cae6aecabca8b476
Rodrigo Alonso <rodrigoalonso@roagro.com.ar>
2024-07-09 11:38:28 -0300

    Instalacion Material-Ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  }
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
