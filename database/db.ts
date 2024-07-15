90f1fd7e6247e446a458ed5d5f1abd08bc2cb2d7
Rodrigo Alonso <rodrigoalonso@roagro.com.ar>
2024-07-15 20:48:13 -0300

    Conectarnos a mongo con mongoose

---
+++ database/db.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+/**
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+ */
+
+const mongoConnection = {
+  isConnected: 0
+}
+
+export const connect = async() => {
+
+  if (mongoConnection.isConnected === 1) {
+    console.log('Ya estamos conectados');
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    mongoConnection.isConnected = mongoose.connections[0].readyState;
+
+    if (mongoConnection.isConnected === 1) {
+      console.log('Usando conexión anterior');
+      return;
+    }
+
+    await mongoose.disconnect();
+  }
+
+  await mongoose.connect(process.env.MONGO_CONNECTION_STRING || '');
+  mongoConnection.isConnected = 1;
+  console.log('Conectado a MongoDB');
+}
+
+export const disconnect = async () => {
+
+  if (mongoConnection.isConnected !== 0) return;
+
+  await mongoose.disconnect();
+  console.log('Desconectado de MongoDB');
+}
